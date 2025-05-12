aa4bed6dfa814d9135febb853033e8520a2cab41
sparcut <sparcut@users.noreply.github.com>
2025-05-12 19:04:40 +1000

    Added dummy auth controller

---
+++ BlockPouch/controllers/AuthenticationController.swift
@@ -0,0 +1,18 @@
+//
+//  AuthenticationController.swift
+//  BlockPouch
+//
+//  Created by Harrison on 12/5/2025.
+//
+
+import Foundation
+
+class AuthenticationController {
+    
+    func authenticateLogin(email: String, password: String) -> Bool {
+        // Connect and authenticate credentials
+        
+        // Return result of authentication
+        return true
+    }
+}
